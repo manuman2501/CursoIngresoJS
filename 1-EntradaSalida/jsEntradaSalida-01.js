ab282ab5c03305d9f6c0840e84f0bc8838f76575
manuman2501 <manuman2501@gmail.com>
2023-01-31 12:12:35 -0300

    ejercicios 1 2

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,9 @@
 /*
+Manuel Fernandez
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Esto funciona de maravilla");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,11 @@
 /*
+Manuel Fernandez
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+    let nombrePersona; //Declaración de variable
+	nombrePersona = prompt("¿Cuál es tu nombre?");
+	alert(nombrePersona);
 }
 
